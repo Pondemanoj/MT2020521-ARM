1dcb4bfc1f43fefa678d20b04f2ff66fbf78de01
Pondemanoj <pondemanoj@gmail.com>
2021-08-24 16:49:40 +0530

    Update q2.c

--- assign_one/q2.c
+++ assign_one/q2.c
@@ -1,1 +1,28 @@
+/* 2. Write a C Program to read the 15 whole numbers into an array implement the algo to find the largest
+in the array */
 
+#include<stdio.h>
+
+main()
+{
+	int a[15],max,i;
+	printf("Enter 15 numbers of your choice \n");
+	
+	for (i=0; i<=14; i++)
+	{
+	scanf("%d", &a[i]);
+	
+	}
+	
+	max =a[0];
+	
+	for (i=0; i<=14; i++)
+	{
+		if (a[i] > max)
+			max=a[i];
+		
+	}
+
+	printf("Max number is = %d \n", max);
+
+}
