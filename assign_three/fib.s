68a9ff434d45eca59233bd8206b2afcc20cb413e
Pondemanoj <pondemanoj@gmail.com>
2021-10-31 22:21:14 +0530

    Update fib.s

--- assign_three/fib.s
+++ assign_three/fib.s
@@ -1,1 +1,25 @@
 
+     AREA     fibanocci, CODE, READONLY
+     EXPORT __main
+     IMPORT printMsg
+
+     ENTRY 
+__main  FUNCTION
+		MOV  R7, #10 
+		MOV  R8, #1 
+		MOV  R9, #1 
+		SUB  R7, #1	
+loopforfib	CMP R7, #0	
+		BLE stop	
+		
+		ADD R8, R9	
+		MOV r0, R8
+		BL printMsg
+		EOR R8, R9	
+		EOR R9, R8
+		EOR R8, R9
+		SUB R7, #1	
+		B loopforfib
+stop    B stop 
+	 ENDFUNC
+	 END
